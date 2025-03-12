3a07494b42b5e9de4506e11cab3b4e0a22b5553f
AlexisDardelet <alexis.dardelet@gmail.com>
2025-03-12 23:03:23 +0100

    import modules

--- app/synthese_cv_llm.py
+++ app/synthese_cv_llm.py
@@ -1,3 +1,10 @@
 import os
 import sys
 import streamlit as st
+
+# Définition du chemin du répertoire courant
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '..', 'ia')))
+from ia import read_pdf, analyze_cv
+
+def synthese_cv_llm():
+    st.title("Sythèse du CV via Mistral LLM")
